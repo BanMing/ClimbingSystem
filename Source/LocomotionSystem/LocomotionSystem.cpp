c7818d3594ed5b9e15d93f6ad745dfec1ac3dc5b
wupeng <wupeng_a2484@virtuos.com.cn>
2024-03-11 17:10:26 +0800

    Add template third person project

---
+++ Source/LocomotionSystem/LocomotionSystem.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LocomotionSystem.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LocomotionSystem, "LocomotionSystem" );
+ 
---
+++ Source/LocomotionSystem/LocomotionSystemGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LocomotionSystemGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ALocomotionSystemGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ALocomotionSystemGameMode();
+};
+
+
+
